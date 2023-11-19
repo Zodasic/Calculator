7769fce3a47800dafbb6f231d0ce73e3cbac26ca
Nguyễn Võ Chí Dũng <23560012@gm.uit.edu.vn>
2023-11-19 16:56:14 +0700

    modified:   script.js 	modified:   index.html 	modified:   style.css

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ class Calculator { //Defines the calculator logic
     this.clear()
   }
 
-  clear() {
+clear() { //Clears the current operand, previous operand, and operation.
     this.currentOperand = ''
     this.previousOperand = ''
     this.operation = undefined
   }
 
-  delete() { //Remove last digit (current operand)
+delete() { //Removes the last digit from the current operand.
     this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
 
-  appendNumber(number) {
+appendNumber(number) { //Appends a number or a decimal point to the current operand, preventing multiple decimal points.
     if (number === '.' && this.currentOperand.includes('.')) return
     this.currentOperand = this.currentOperand.toString() + number.toString()
   }
 
-  chooseOperation(operation) { //sets the current based on the operation button onclick
-    if (this.currentOperand === '') return
+chooseOperation(operation) { //sets the current based on the operation button onclick
+   if (this.currentOperand === '') return //If there is a current operand, it computes the result if there is also a previous operand.
     if (this.previousOperand !== '') {
       this.compute()
     }
@@ -30,10 +30,11 @@ class Calculator { //Defines the calculator logic
     this.currentOperand = ''
   }
 
-  compute() { //Performs operation using current and previous operands
+compute() { //Performs operation using current and previous operands
     let computation
     const prev = parseFloat(this.previousOperand)
     const current = parseFloat(this.currentOperand)
+    //Updates the current operand with the result.
     if (isNaN(prev) || isNaN(current)) return
     switch (this.operation) {
       case '+':
@@ -56,7 +57,7 @@ class Calculator { //Defines the calculator logic
     this.previousOperand = ''
   }
 
-  getDisplayNumber(number) { //Formats a number as a string to display
+getDisplayNumber(number) { //Formats a number as a string for display, handling integer and decimal parts separately.
     const stringNumber = number.toString()
     const integerDigits = parseFloat(stringNumber.split('.')[0])
     const decimalDigits = stringNumber.split('.')[1]
@@ -73,7 +74,7 @@ class Calculator { //Defines the calculator logic
     }
   }
 
-  updateDisplay() { //Display current and previous operands
+updateDisplay() { //Display current and previous operands
     this.currentOperandTextElement.innerText =
       this.getDisplayNumber(this.currentOperand)
     if (this.operation != null) {
@@ -86,6 +87,7 @@ class Calculator { //Defines the calculator logic
 }
 
 //Reference to present the button on the calculator
+//Selects HTML elements representing number buttons, operation buttons, equals button, delete button, clear button, and text elements for displaying operands.
 const numberButtons = document.querySelectorAll('[data-number]') 
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -96,13 +98,27 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-acceptedKeys=[1,2,3,4,5,6,7,8,9,0]
-window.addEventListener('keydown',function(e){
-  if(!acceptedKeys.includes(e.key)){
-    calculator.appendNumber(e.key)
-    calculator.updateDisplay()
+//The acceptedKeys array is used to filter out valid number keys (0-9).
+acceptedKeys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') { //The Enter key triggers the compute method to perform the calculation, and then the updateDisplay method is called to update the display.
+    calculator.compute();
+    calculator.updateDisplay();
+
+  } else if (e.key === 'Backspace') { //The Backspace key triggers the delete method to remove the last digit.
+    calculator.delete();
+    calculator.updateDisplay();
+
+  } else if (e.key === '*' || e.key === '/' || e.key === '+' || e.key === '-') {//The *, /, +, and - keys trigger the chooseOperation method to set the operation based on the pressed key.
+    calculator.chooseOperation(e.key);
+    calculator.updateDisplay();
+
+  } else if (!acceptedKeys.includes(e.key) && e.key !== '.') {
+    calculator.appendNumber(e.key);
+    calculator.updateDisplay();
   }
-})
+});
+
 //This loop iterates over each of the buttons and adds an event listener to handle the button click event.
 numberButtons.forEach(button => { 
   button.addEventListener('click', () => {
